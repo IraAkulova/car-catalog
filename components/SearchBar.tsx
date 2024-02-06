cb1c3537810b6f80335d6b7a0ab363fe59d5205f
Ira Akulova <99876200+IraAkulova@users.noreply.github.com>
2024-02-06 15:01:13 +0200

    added modal, added input

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,11 +1,23 @@
 'use client';
 import { useState } from "react";
-
-import { SearchManufacturer } from "."
+import Image from "next/image";
+import { SearchManufacturer } from ".";
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src={"/magnifying-glass.svg"}
+      alt={"magnifying glass"}
+      width={40}
+      height={40}
+      className='object-contain'
+    />
+  </button>
+);
 
 function SearchBar() {
     const [manufacturer, setManufacturer] = useState('');
-    const handleSearch = () => {
+  const [model, setModel] = useState('');  
+  const handleSearch = () => {
         
     }
   return (
@@ -14,8 +26,26 @@ function SearchBar() {
             <SearchManufacturer
                 manufacturer={manufacturer}
                 setManufacturer={setManufacturer}  
-            />
-        </div>
+        />
+        <SearchButton otherClasses='sm:hidden'/>
+      </div>
+      <div className="searchbar__item">
+        <Image
+          src='/model-icon.png'
+          width={25}
+          height={25}
+          className='absolute w-[20px] h-[20px] ml-4'
+          alt='car model'
+        />
+        <input
+          type='text'
+          name='model'
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder='Tiguan'
+          className='searchbar__input'
+        />
+      </div>
     </form>
   )
 }
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
     const headers = {
 		'X-RapidAPI-Key': '13a26341a1msh1a61b4d6d68b2c8p176d86jsn625aaa9e6c6a',
@@ -24,3 +26,16 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 
   return rentalRatePerDay.toFixed(0);
 };
+
+export const generateCarImageUrl = ( car: CarProps, angle?: string) => {
+  const url = new URL('https://cdn.imagin.studio/getimage');
+  const { make, year, model } = car;
+  url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(" ")[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
+}
